a5e884f8c7c5c019adf9bd8fdc4c496df32245ae
sallyschoolaccount <salvatorepontillo07@gmail.com>
2025-02-05 12:24:22 +0100

    Connection Estabilishedw

--- Homepage.php
+++ Homepage.php
@@ -1,10 +1,19 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "books");
+$result = mysqli_query($conn, "SELECT * FROM buecher");
+$row = mysqli_fetch_assoc($result)
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="UTF-8">
   <title>Antiquariat Bieber</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous">
 </head>
 <body>
 
@@ -36,19 +45,25 @@
 <div class="container px-4 py-5" id="custom-cards">
   <h2 class="pb-2 border-bottom">Custom cards</h2>
 
+  <!--Bestseller 1-->
   <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
     <div class="col">
       <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
         <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Buch 1</h3>
+
+            <!--Name 1-->
+          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold"> Buch 1</h3>
           <ul class="d-flex list-unstyled mt-auto">
             <li class="me-auto">
               <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
             </li>
+
+              <!--Autor 1-->
             <li class="d-flex align-items-center me-3">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
               <small>Autor 1</small>
             </li>
+
             <li class="d-flex align-items-center">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
               <small></small>
@@ -58,18 +73,24 @@
       </div>
     </div>
 
+    <!--Bestseller 2-->
     <div class="col">
       <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-2.jpg');">
         <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+
+            <!--Name 2-->
           <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Buch 2</h3>
           <ul class="d-flex list-unstyled mt-auto">
             <li class="me-auto">
               <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
             </li>
+
+              <!--Autor 2-->
             <li class="d-flex align-items-center me-3">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
               <small>Autor 2</small>
             </li>
+
             <li class="d-flex align-items-center">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
               <small></small>
@@ -79,18 +100,24 @@
       </div>
     </div>
 
+    <!--Bestseller 3-->
     <div class="col">
       <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
         <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+
+            <!--Name 3-->
           <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Buch3</h3>
           <ul class="d-flex list-unstyled mt-auto">
             <li class="me-auto">
               <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
             </li>
+
+              <!--Autor 3-->
             <li class="d-flex align-items-center me-3">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
               <small>Autor3</small>
             </li>
+
             <li class="d-flex align-items-center">
               <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
               <small></small>
